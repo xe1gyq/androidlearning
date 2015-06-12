ab6086731673a6fde5a07e93f736925ae3698447
Abraham Arce <xe1gyq@gmail.com>
2015-06-12 15:55:12 -0500

    Android Learning Base Code
    Base code for Android Learning Repository
    
    Signed-off-by: Abraham Arce <xe1gyq@gmail.com>

---
+++ training/NewCircle/Balamex/src/com/example/balamex/BalamexLib.java
@@ -0,0 +1,18 @@
+package com.example.balamex;
+
+public class BalamexLib {
+	
+	public static long balamatorJOn(long status) {
+		return 1;
+	}
+	public native static long balamatorIOn(long status);
+	
+	public static long balamatorJOff(long status) {
+		return 0;
+	}
+	public native static long balamatorIOff(long status);
+	
+	static {
+		System.loadLibrary("com_example_balamex_BalamexLib");
+	}
+}
